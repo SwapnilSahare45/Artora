9d8595c404d6631e15c7fdc2fb1c34747ca73dcc
Swapnil Sahare <swapnilsahare45@gmail.com>
2025-07-30 05:38:24 +0530

    fix view auction button logic

--- client/src/components/AuctionCard.jsx
+++ client/src/components/AuctionCard.jsx
@@ -10,12 +10,21 @@ const AuctionCard = ({ id, title, startDate, endDate }) => {
                 Ends: {new Date(endDate).toDateString()}
             </p>
 
-            <Link
-                to={`/auction/${id}`}
-                className="inline-block bg-primary text-white px-4 py-2 rounded hover:bg-opacity-90 transition"
-            >
-                View Auction
-            </Link>
+            {
+                new Date() < new Date(startDate) ? (
+                    <p className="text-gray-400">Auction not start yet.</p>
+                ) :
+                    new Date() > new Date(endDate) ? (
+                        <p className="text-gray-400">Auction has ended.</p>
+                    ) : (
+                        <Link
+                            to={`/auction/${id}`}
+                            className="inline-block bg-primary text-white px-4 py-2 rounded hover:bg-opacity-90 transition"
+                        >
+                            View Auction
+                        </Link>
+                    )
+            }
         </div>
     )
 }
