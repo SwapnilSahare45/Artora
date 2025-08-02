331e7964f6a25044f9cf3e40459e4b8f32e9f8b7
Swapnil Sahare <swapnilsahare45@gmail.com>
2025-08-02 12:54:26 +0530

    done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -90,7 +90,7 @@ const Home = () => {
       <section className='pb-12'>
         <h3 className='text-4xl font-semibold mb-8 text-center'>What Our Users Say</h3>
         <div className='grid grid-cols-1 gap-4 px-4 md:grid-cols-3 md:px-8 lg:px-24'>
-          {feedbacks &&
+          {feedbacks && feedbacks.user &&
             feedbacks.map((feedback) => (
               <TestimonialCard
                 key={feedback._id}
